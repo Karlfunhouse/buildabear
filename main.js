754ab9ae81c8c4021d2213c5da027fe8c627a802
Lane Hunter <lanehunter.96@gmail.com>
2019-12-16 13:34:23 -0700

    :building_construction: :crystal_ball: Define global variables for outfits and added js classes to html

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 var newOutfit = new Outfit(null, null, null, null);
+var hatOptions = document.querySelector('.hat-options-js');
+var clothesOptions = document.querySelector('.clothes-options-js');
+var accessoriesOptions = document.querySelector('.accessories-options-js');
+var backgroundOptions = document.querySelector('.background-options-js');
 console.log(newOutfit);
+
+hatOptions.addEventListener('click', selectHat);
+clothesOptions.addEventListener('click', selectClothes);
+accessoriesOptions.addEventListener('click', selectAccessories);
+backgroundOptions.addEventListener('click', selectBackground);
+
+function selectHat() {
+  console.log('poop1');
+}
+function selectClothes() {
+  console.log('poop2');
+}
+function selectAccessories() {
+  console.log('poop3');
+}
+function selectBackground() {
+  console.log('poop4');
+};
