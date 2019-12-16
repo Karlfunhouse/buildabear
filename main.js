c04c93bb38664c5f18d96c5e210be600ca6b3a7d
Lane Hunter <lanehunter.96@gmail.com>
2019-12-16 16:28:39 -0700

    :crystal_ball: Add hat buttons event listeners

--- main.js
+++ main.js
@@ -11,7 +11,16 @@ accessoriesOptions.addEventListener('click', selectAccessories);
 backgroundOptions.addEventListener('click', selectBackground);
 
 function selectHat() {
-  console.log('poop1');
+  if (event.target.classList.contains('top-hat-js')) {
+    console.log('tophat');
+  } else if (event.target.classList.contains('sun-hat-js')) {
+    console.log('sunhat');
+  } else if (event.target.classList.contains('bow-js')) {
+    console.log('bow');
+  } else if (event.target.classList.contains('crown-js')) {
+    console.log('crown');
+  }
+  
 }
 function selectClothes() {
   console.log('poop2');
