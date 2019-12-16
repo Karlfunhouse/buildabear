549efc3160a8eba11ba3f2aba562b56cdc9ebe28
Karl Nielsen <karl.g.nielsen@gmail.com>
2019-12-16 10:49:28 -0700

    :crystal_ball: Add outfit class and parameters & properties; addClothing & removeClothing functions created but not finished

--- outfit.js
+++ outfit.js
@@ -1,14 +1,18 @@
 class Outfit {
-  constructor () {
+  constructor (id, title, garments, background) {
+    this.id = id;
+    this.title = title;
+    this.garments = garments;
+    this.background = background;
 
   }
 
-  addGarment() {
-
+  addGarment(newGarment) {
+    this.garments = this.garments.push(newGarment);
   }
 
-  removeGarment() {
-    
+  removeGarment(removeGarment) {
+    this.garments = this.garments.splice(removeGarment);
   }
 }
 
