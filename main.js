efafff993a9b7f6a27e0755ab10bbc766a13485d
Karl Nielsen <karl.g.nielsen@gmail.com>
2019-12-17 12:51:17 -0700

    :crystal_ball: Refactored all the event listener functions to be able to select and deselect an item and have that accurately be reflected in the garments array

--- main.js
+++ main.js
@@ -35,61 +35,61 @@ function selectHat() {
 }
 
 function selectClothes() {
-  var activeClothes = clothesOptions.querySelector('.active-item');
-  removeActiveItem(activeClothes);
-  newOutfit.removeGarment(activeClothes);
-  if (event.target.classList.contains('vest-js')) {
-    newOutfit.addGarment('vest');
+  if (event.target != event.currentTarget && !event.target.classList.contains('active-item')) {
+    var activeClothes = clothesOptions.querySelector('.active-item');
+    removeActiveItem(activeClothes);
     addActiveItem(event.target);
-  } else if (event.target.classList.contains('dress-js')) {
-    newOutfit.addGarment('dress');
-    addActiveItem(event.target);
-  } else if (event.target.classList.contains('tuxedo-js')) {
-    newOutfit.addGarment('tuxedo');
-    addActiveItem(event.target);
+    if (activeClothes != null) {
+      newOutfit.removeGarment(activeClothes.value);
+    }
+    newOutfit.addGarment(event.target.value);
+  } else {
+    event.target.classList.remove('active-item');
+    newOutfit.removeGarment(event.target.value);
   }
 }
+// function selectClothes() {
+//   var activeClothes = clothesOptions.querySelector('.active-item');
+//   removeActiveItem(activeClothes);
+//   newOutfit.removeGarment(activeClothes);
+//   if (event.target.classList.contains('vest-js')) {
+//     newOutfit.addGarment('vest');
+//     addActiveItem(event.target);
+//   } else if (event.target.classList.contains('dress-js')) {
+//     newOutfit.addGarment('dress');
+//     addActiveItem(event.target);
+//   } else if (event.target.classList.contains('tuxedo-js')) {
+//     newOutfit.addGarment('tuxedo');
+//     addActiveItem(event.target);
+//   }
+// }
 
 function selectAccessories() {
-  var activeAccessories = accessoriesOptions.querySelector('.active-item');
-  removeActiveItem(activeAccessories);
-  newOutfit.removeGarment(activeAccessories);
-  if (event.target.classList.contains('necklace-js')) {
-    newOutfit.addGarment('necklace');
+  if (event.target != event.currentTarget && !event.target.classList.contains('active-item')) {
+    var activeAccessories = accessoriesOptions.querySelector('.active-item');
+    removeActiveItem(activeAccessories);
     addActiveItem(event.target);
-  } else if (event.target.classList.contains('bowtie-js')) {
-    newOutfit.addGarment('bowtie');
-    addActiveItem(event.target);
-  } else if (event.target.classList.contains('watch-js')) {
-    newOutfit.addGarment('watch');
-    addActiveItem(event.target);
-  } else if (event.target.classList.contains('space-helmet-js')) {
-    newOutfit.addGarment('space-helmet');
-    addActiveItem(event.target);
-  } else if (event.target.classList.contains('earrings-js')) {
-    newOutfit.addGarment('earrings');
-    addActiveItem(event.target);
+    if (activeAccessories != null) {
+      newOutfit.removeGarment(activeAccessories.value);
+    }
+    newOutfit.addGarment(event.target.value);
+  } else {
+    event.target.classList.remove('active-item');
+    newOutfit.removeGarment(event.target.value);
   }
 }
 
 function selectBackground() {
-  var activeBackground = backgroundOptions.querySelector('.active-item');
-  removeActiveItem(activeBackground);
-  newOutfit.removeGarment(activeBackground);
-  if (event.target.classList.contains('sky-js')) {
-    newOutfit.addGarment('sky');
+  if (event.target != event.currentTarget && !event.target.classList.contains('active-item')) {
+    var activeBackground = backgroundOptions.querySelector('.active-item');
+    removeActiveItem(activeBackground);
     addActiveItem(event.target);
-  } else if (event.target.classList.contains('sunset-js')) {
-    newOutfit.addGarment('sunset');
-    addActiveItem(event.target);
-  } else if (event.target.classList.contains('park-js')) {
-    newOutfit.addGarment('park');
-    addActiveItem(event.target);
-  } else if (event.target.classList.contains('beach-js')) {
-    newOutfit.addGarment('beach');
-    addActiveItem(event.target);
-  } else if (event.target.classList.contains('space-js')) {
-    newOutfit.addGarment('space');
-    addActiveItem(event.target);
+    if (activeBackground != null) {
+      newOutfit.removeGarment(activeBackground.value);
+    }
+    newOutfit.addGarment(event.target.value);
+  } else {
+    event.target.classList.remove('active-item');
+    newOutfit.removeGarment(event.target.value);
   }
 };
