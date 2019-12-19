4f28d21b4916821c0eebee18584c6a377f890e8e
Ryan Bahan <ryan@ryanbahan.com>
2019-12-18 21:00:13 -0700

    :ambulance: Actually comment out the comment instead of leaving it wide open in the wild

--- main.js
+++ main.js
@@ -179,11 +179,11 @@ function storeOutfit(outfit) {
   window.localStorage.setItem(outfit.title, JSON.stringify(outfit));
 }
 
-Loop through the entire length of the localStorage array,
-Grab the key of each item in the array,
-Call the getItem method to retrieve each item's data by using our respective localStorage key, 
-Turn the data from a string back into an object using JSON.parse,
-Push the retrieved object into our saved outfits array!
+// Loop through the entire length of the localStorage array,
+// Grab the key of each item in the array,
+// Call the getItem method to retrieve each item's data by using our respective localStorage key,
+// Turn the data from a string back into an object using JSON.parse,
+// Push the retrieved object into our saved outfits array!
 function getOutfits() {
   for (var i = 0; i < localStorage.length; i++) {
     var outfitKey = localStorage.key([i]);
