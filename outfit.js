e73bf468c2075fd41709283e41115d620b4ef9a6
Ryan Bahan <ryan@ryanbahan.com>
2019-12-27 17:23:08 -0700

    :crystal_ball: Update removeGarments function to be more dynamic

--- outfit.js
+++ outfit.js
@@ -4,15 +4,19 @@ class Outfit {
     this.title = title;
     this.garments = garments;
     this.background = background;
-
   }
 
-  addGarment(garment) {
+  addGarment(garment, type) {
     this.garments.push(garment);
   }
-
-  removeGarment(garment) {
-    var removedItem = this.garments.indexOf(garment);
-    this.garments.splice(removedItem, 1);
+  // removeGarment(garment) {
+  //   var removedItem = this.garments.indexOf(garment);
+  //   this.garments.splice(removedItem, 1);
+  // }
+  removeGarment(type) {
+    var removedItem = this.garments.find(item => item.type == type);
+    if (removedItem != undefined) {
+      this.garments.splice(removedItem, 1);
+    }
   }
 }
