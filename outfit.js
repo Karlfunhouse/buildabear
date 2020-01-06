f1d0790c37a68e68e1790200731dee02d8a708b1
Lane Hunter <lanehunter.96@gmail.com>
2020-01-06 16:21:53 -0700

    :crystal_ball: Refactor outift class and addGarment function

--- outfit.js
+++ outfit.js
@@ -1,18 +1,9 @@
 class Outfit {
-  constructor (id, title, garments, background) {
-    this.id = id;
-    this.title = title;
-    this.garments = garments;
-    this.background = background;
-
-  }
-
-  addGarment(garment) {
-    this.garments.push(garment);
-  }
-
-  removeGarment(garment) {
-    var removedItem = this.garments.indexOf(garment);
-    this.garments.splice(removedItem, 1);
-  }
+ constructor (id, title, garments, background) {
+  this.id = id;
+  this.title = title;
+  this.garments = garments;
+  this.background = background;
+ }
+ addGarment() {
 }
