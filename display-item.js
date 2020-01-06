d46f193571c7770c5587f93377e56f35391f0061
Lane Hunter <lanehunter.96@gmail.com>
2020-01-06 16:13:56 -0700

    :crystal_ball: Add new functions that check the type of item and active status

--- display-item.js
+++ display-item.js
@@ -5,6 +5,7 @@ class DisplayItem {
    this.imageId = document.getElementById(`${item.target.value}`);
    this.buttonId = item.target;
   }
+
   // Sort backgrounds from garments, initiating the control flow for
   // whether the item passed in gets selected or deselected.
   update() {
@@ -23,3 +24,35 @@ class DisplayItem {
       newOutfit.addBackground(this);
     }
   }
+  // Check if the garment passed in is currently active.
+ // If it is, disable it. If not, check the garment category.
+ checkActiveGarment() {
+   if (newOutfit.garments.find
+     (garment => garment.id === this.id) !== undefined) {
+       newOutfit.removeGarment(this);
+   } else {
+     this.checkActiveGarmentCategory();
+   }
+ }
+ // Check if the background category already has an active item.
+ // If it does, disable it. Either way, add the new background.
+ checkBackgroundCategory() {
+   if (newOutfit.background.id === this.id) {
+     newOutfit.removeBackground();
+   } else {
+     newOutfit.removeBackground();
+     newOutfit.addBackground(this);
+   }
+ }
+ // Check if the garment passed in has an active item in its respective category.
+ // If it does, disable that item. Either way, enable the new item.
+ checkActiveGarmentCategory() {
+   if (newOutfit.garments.find(
+     garment => garment.type === this.type) !== undefined) {
+     var lastActive = newOutfit.garments.find(
+       garment => garment.id !== this.id && garment.type === this.type);
+     newOutfit.removeGarment(lastActive);
+   }
+   newOutfit.addGarment(this);
+ }
+}
