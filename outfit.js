3c9d907589ed0a1f2726612549a43b1c68a7e540
Lane Hunter <lanehunter.96@gmail.com>
2020-01-06 16:25:31 -0700

    :recycle: Refactor removeGarment function

--- outfit.js
+++ outfit.js
@@ -9,5 +9,11 @@ class Outfit {
   this.garments.push(garment);
   garment.imageId.classList.remove('hidden');
   garment.buttonId.classList.add('active-item');
-  }
+ }
+ removeGarment(garment) {
+  var removedItem = this.garments.find(item => item.type === garment.type);
+  this.garments = this.garments.filter(item => item.id != removedItem.id);
+  garment.imageId.classList.add('hidden');
+  garment.buttonId.classList.remove('active-item');
+ }
 }
