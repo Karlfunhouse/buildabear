d70ef771d33c1877b2821e4be90b1348e79b9d73
Ryan Bahan <ryan@ryanbahan.com>
2020-01-07 10:24:33 -0700

    :crystal_ball: Add iteration 5 functionality

--- outfit.js
+++ outfit.js
@@ -30,5 +30,58 @@ class Outfit {
   this.background.imageId.classList.add('hidden');
   this.background = null;
  }
- 
+
+ update(outfit) {
+    this.getDOMElements(outfit);
+
+    this.id = outfit.id;
+    this.title = outfit.title;
+    this.garments = outfit.garments;
+    this.background = outfit.background;
+
+    this.garments.forEach(garment => this.addGarment(garment));
+    document.querySelector('.outfit-name-input-js').value = `${this.title}`;
+
+    if (outfit.background !== null) {
+        this.addBackground(outfit.background);
+    }
+ }
+
+getDOMElements(outfit) {
+    outfit.garments.forEach(item => item.buttonId = document.querySelector(`button[value=${item.id}]`));
+    outfit.garments.forEach(item => item.imageId = document.getElementById(`${item.id}`));
+  
+    if (outfit.background !== null) {
+      outfit.background.imageId = document.getElementById(`${outfit.background.id}`);
+      outfit.background.buttonId = document.querySelector(`button[value=${outfit.background.id}]`);
+    }
+}
+reset() {
+    newOutfit = new Outfit(Date.now(), null, [], null);
+    var images = document.querySelectorAll('.image-absolute');
+    var activeButtons = document.querySelectorAll('.active-item');
+    images.forEach(image => image.classList.add('hidden'))
+    activeButtons.forEach(button => button.classList.remove('active-item'));
+    saveForm.reset();
+    checkFormValidity();
+}
+storeOutfit() {
+    if (savedOutfits.find(outfit => outfit.title === this.title) === undefined) {
+        this.storeNewOutfit();
+    } else {
+        this.updateStoredOutfit();
+    }
+}
+storeNewOutfit() {
+    this.title = document.querySelector('.outfit-name-input-js').value;
+    savedOutfits.push(this);
+    displayNewOutfitCard();
+    window.localStorage.setItem('outfits', JSON.stringify(savedOutfits));
+}
+updateStoredOutfit() {
+    var outfit = savedOutfits.findIndex(outfit => outfit.title === this.title);
+    savedOutfits.splice(outfit, 1);
+    savedOutfits.push(this);
+    window.localStorage.setItem('outfits', JSON.stringify(savedOutfits));
+}
 }
